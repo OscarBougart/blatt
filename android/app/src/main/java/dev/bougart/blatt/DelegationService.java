e41b8caf74fce2f355f74a6ef3720e94d56fe6eb
Oscar Carbonell <oscarcarbonell9292@gmail.com>
2026-09-20 21:48:05 +0200

    gitignore

---
+++ android/app/src/main/java/dev/bougart/blatt/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.bougart.blatt;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
